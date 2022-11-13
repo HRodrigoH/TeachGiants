927551deffc3b417d5d33a79d942f1c514256ec8
Manuel Bejar B <86585679+leunamnevets@users.noreply.github.com>
2022-11-13 16:11:38 -0500

    Update chatgui.py

--- Desarrollo/CCNT/Backend/chatgui.py
+++ Desarrollo/CCNT/Backend/chatgui.py
@@ -92,7 +92,6 @@ base.title("Hello")
 base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
-#Create Chat window
 ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
 
 ChatLog.config(state=DISABLED)
